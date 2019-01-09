5ca63f39b34d82ffc9166f860cd9cb9164db8dc1
lizexing <1013926837@qq.com>
2019-01-09 16:22:36 +0800

    初始化导入项目，这个是我刚创建的

---
+++ src/main/java/com/example/demoddddd/DemodddddApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoddddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodddddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemodddddApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/example/demoddddd/DemodddddApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demoddddd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemodddddApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
